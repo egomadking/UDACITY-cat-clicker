05f3c4d93d32e5b8f843f63a0dca5debba3c84f9
Roy Mosby <ego.madking@gmail.com>
2017-12-23 23:00:46 -0500

    object containing cats values, function to add DOM elements and cat content, event listener on #main that listens for clicks on images, function to increment click counter

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+// When loaded, the event listener
+// listens for the cat
+
+var cat = [{
+  'name': 'Whiskers',
+  'url': 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+  'clickCount': 0
+  },
+  {
+  'name': 'Blued-eyes',
+  'url': 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
+  'clickCount': 0
+  }
+];
+
+var main = document.getElementById('main');
+
+function addCats(cat) {
+  var card = '<div class="card"></div>';
+  var catCount = cat.length;
+  for(var i = 0; i < catCount; i++) {
+    main.insertAdjacentHTML('beforeend', card);
+    var catCard = document.getElementsByClassName('card');
+    var catName = '<div class="catName"><h2>' + cat[i].name + '</h2></div>';
+    var catPic = '<div class="catPicture"><img data-cat-number="' + [i] + '" src="' + cat[i].url + '"></div>';
+    var clickCounterDispl = '<div class="clickCounter">Number of times clicked: ' +
+      '<span id="clickCounter' +[i] + '">0</span>';
+    catCard[i].insertAdjacentHTML('beforeend',catName + catPic + clickCounterDispl);
+  }
+}
+
+addCats(cat);
+
+main.addEventListener('click', addCounter, false);
+
+function addCounter(e) {
+  if(e.target.localName === 'img') {
+    num = e.target.attributes['data-cat-number'].nodeValue;
+    cat[num].clickCount++;
+    incrementCounter(num);
+  }
+}
+
+function incrementCounter(e) {
+  var counter = 'clickCounter' + e;
+  document.getElementById(counter).innerHTML = cat[e].clickCount;
+}
