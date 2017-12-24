e1afce8e4630d7dbe075e8322884f4919728f68d
Roy Mosby <ego.madking@gmail.com>
2017-12-24 17:24:34 -0500

    changed addCats to add cats to selector div, stageCat function to move selected cat to stage, event listener on selector to stage selected cat.

--- scripts/app.js
+++ scripts/app.js
@@ -29,24 +29,40 @@ var cat = [{
 ];
 
 var main = document.getElementById('main');
+var selector = document.getElementById('selector');
 
 function addCats(cat) {
-  var card = '<div class="card"></div>';
+  var miniCard = '<div class="miniCard"></div>';
   var catCount = cat.length;
   for(var i = 0; i < catCount; i++) {
-    main.insertAdjacentHTML('beforeend', card);
-    var catCard = document.getElementsByClassName('card');
-    var catName = '<div class="catName"><h2>' + cat[i].name + '</h2></div>';
-    var catPic = '<div class="catPicture"><img data-cat-number="' + [i] + '" src="' + cat[i].url + '"></div>';
-    var clickCounterDispl = '<div class="clickCounter">Number of times clicked: ' +
-      '<span id="clickCounter' +[i] + '">0</span>';
-    catCard[i].insertAdjacentHTML('beforeend',catName + catPic + clickCounterDispl);
+    selector.insertAdjacentHTML('beforeend', miniCard);
+    var catCard = document.getElementsByClassName('miniCard');
+    var catName = '<div class="catName"><h3>' + cat[i].name + '</h3></div>';
+    var catThumb = '<div class="catPrev"><img data-cat-thumb-number="' + [i] + '" class="catThumb" src="' + cat[i].url + '"></div>';
+    catCard[i].insertAdjacentHTML('beforeend',catName + catThumb);
   }
 }
-/*
+
 addCats(cat);
 
-main.addEventListener('click', addCounter, false);
+var stage = document.getElementById('stage');
+
+// where e is the [data-thumb-number].nodeValue
+// found inside addCounter()
+function stageCat(e) {
+  var card = '<div class="card"></div>';
+  stage.innerHTML = card;
+  var catCard = document.getElementsByClassName('card');
+  var catName = '<div class="catName"><h2>' + cat[e].name + '</h2></div>';
+  var catPic = '<div class="catPicture"><img data-cat-number="' + [e] + '" src="' + cat[e].url + '"></div>';
+  var clickCounterDispl = '<div class="clickCounter">Number of times clicked: ' +
+    '<span id="clickCounter' +[e] + '">' + cat[e].clickCount + '</span>';
+  catCard[0].insertAdjacentHTML('beforeend',catName + catPic + clickCounterDispl);
+}
+
+stageCat(1);
+
+stage.addEventListener('click', addCounter, false);
 
 function addCounter(e) {
   if(e.target.localName === 'img') {
@@ -60,4 +76,15 @@ function incrementCounter(e) {
   var counter = 'clickCounter' + e;
   document.getElementById(counter).innerHTML = cat[e].clickCount;
 }
-*/
+
+selector.addEventListener('click', addSelector, false);
+
+function addSelector(e) {
+  num = e.target.attributes['data-cat-thumb-number'].nodeValue;
+  stageCat(num);
+}
+
+// Need event listener on stage
+// that takes the item clicked and
+// replaces the currently staged
+// cat
